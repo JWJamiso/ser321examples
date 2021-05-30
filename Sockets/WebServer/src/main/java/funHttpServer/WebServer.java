22e33dc5c8c6a58d7f2944e844916b034204bb97
John Jamison <johnjamison@FanSideds-MacBook-Pro.local>
2021-05-29 17:10:19 -0700

    made syntax changes

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -213,7 +213,7 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Result is: " + result);
-          } catch (Expection e) {
+          } catch (Exception e) {
             //failure case
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
