3cba6cc0b1a2fed675b4c2a341179397ca5167a0
John Jamison <johnjamison@FanSideds-MacBook-Pro.local>
2021-05-30 14:45:48 -0700

    changed parser input

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -245,7 +245,7 @@ class WebServer {
           // amehlhase, 46384989 -> test316
           try {
             JSONParser parser = new JSONParser();
-            Object object = parser.parse(json);
+            Object object = parser.parse(new FileReader(json));
             JSONArray jsonArray = (JSONArray) object;
             //System.out.println(jsonArray);
             for (Object o : jsonArray) {
