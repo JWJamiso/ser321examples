9cb746f764092fcc8c94ea69452a89b4ae96cde5
John Jamison <johnjamison@fansideds-mbp.lan>
2021-05-30 17:17:50 -0700

    Another test run

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -253,7 +253,7 @@ class WebServer {
               JSONObject jsonObject = (JSONObject) o;
               JSONObject owner = (JSONObject) jsonObject.get("owner");
               String loginName = (String) owner.get("login");
-              String ownerId = (String) owner.get("id");
+              Integer ownerId = (Integer) owner.get("id");
               String repoName = (String) jsonObject.get("name");
               System.out.print(loginName + ", " + ownerId + "-> " + repoName);
               //System.out.println(repoName);
