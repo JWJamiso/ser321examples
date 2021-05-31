a1217119e9f60df6ffad98bc70bfd7f3bccafd86
John Jamison <johnjamison@fansideds-mbp.lan>
2021-05-30 17:20:12 -0700

    Fixed data type

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -253,9 +253,9 @@ class WebServer {
               JSONObject jsonObject = (JSONObject) o;
               JSONObject owner = (JSONObject) jsonObject.get("owner");
               String loginName = (String) owner.get("login");
-              Integer ownerId = (Integer) owner.get("id");
+              Integer ownerId = owner.get("id");
               String repoName = (String) jsonObject.get("name");
-              System.out.print(loginName + ", " + ownerId + "-> " + repoName);
+              System.out.print(loginName + ", " + ownerId.toString() + "-> " + repoName);
               //System.out.println(repoName);
             }
           } catch (Exception e) {
